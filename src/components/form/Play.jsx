6298d2d44858cd5c408c58f835d2e9df3e3ffa95
alfredoizjr <alfredo.alucar@gmail.com>
2021-05-05 09:37:17 -0400

    fixed some style

--- src/components/form/Play.jsx
+++ src/components/form/Play.jsx
@@ -1,13 +1,17 @@
+import { useState } from "react";
 import "./Play.scss";
 import { useAppContext } from "context/AppContext";
 
 export const Play = () => {
+
   const { keyLogs, updaterKeysCtx } = useAppContext();
+  const [loading, setLoading] = useState(false);
 
   const handlePlayLogs = () => {
     updaterKeysCtx({
       selected: null,
     });
+    setLoading(true);
     let count = 0;
     const intervalId = setInterval(() => {
       updaterKeysCtx({
@@ -17,13 +21,18 @@ export const Play = () => {
 
       if (count === keyLogs.length) {
         clearInterval(intervalId);
+        setLoading(false);
       }
     }, 1000);
   };
 
   return (
-    <button onClick={handlePlayLogs} className='play'>
-      Play
-    </button>
+    <>
+      {keyLogs.length !== 0 && (
+        <button disabled={loading} onClick={handlePlayLogs} className='play'>
+          Play
+        </button>
+      )}
+    </>
   );
 };
